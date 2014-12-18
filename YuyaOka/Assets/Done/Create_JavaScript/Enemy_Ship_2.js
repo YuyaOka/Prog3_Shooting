3fb7c77e0b51422ef6b8c60500850c71d04624b9
YuyaOka <oka.03_05@me.com>
2014-12-18 14:13:00 +0900

    【YuyaOka】フォルダ追加
    機能も多少追加

---
+++ YuyaOka/Assets/Done/Create_JavaScript/Enemy_Ship_2.js
@@ -0,0 +1,16 @@
+#pragma strict
+
+var Enemy : Transform ;
+
+function Update ()
+{
+  if(Time.frameCount % 120 == 0)
+  {
+    Instantiate(Enemy , Vector3(Random.Range(19.5 , -19.5) , 0 ,13) , transform.rotation) ;
+  }
+}
+
+function OnCollisonEnter()
+{
+  Application.LoadLevel("GameOver") ;
+}
