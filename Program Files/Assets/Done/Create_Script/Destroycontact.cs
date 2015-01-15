40dd752dc3b3cb158302081b9b3d8eefec986fde
YuyaOka <oka.03_05@me.com>
2015-01-15 12:01:30 +0900

    フォルダ削除

---
+++ Program Files/Assets/Done/Create_Script/Destroycontact.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroycontact : MonoBehaviour {
+
+	public int scoreValue;
+	private GameController gameController;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
