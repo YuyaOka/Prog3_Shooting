43d523fc10d5aabd28e9c5812f4345136c64d040
Yuya Oka <YuyaOka@users.noreply.github.com>
2015-01-27 11:13:45 +0900

    Update GameOver.js

--- Assets/Shooting_Game/Create_JavaScript/GameOver.js
+++ Assets/Shooting_Game/Create_JavaScript/GameOver.js
@@ -11,7 +11,7 @@ function Start() {
 
 function OnGUI()
 {
-  GUI.Label(Rect(Screen.width / 2 - 100 , Screen.height / 2 - 70 , 200 , 50) , "RESULT\nSCORE : " + Score.Score  + "pt   " + "DESTROY : " + Counter.Count , customLabel) ;
+  GUI.Label(Rect(Screen.width / 2 - 100 , Screen.height / 2 - 70 , 200 , 50) , "RESULT\nDESTROY : " + Counter.Count , customLabel) ;
 }
 
 function Update() {
