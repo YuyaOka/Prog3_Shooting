fbfa178571d1cb6e835718eee86c4e20a9c5aa18
YuyaOka <oka.03_05@me.com>
2015-01-29 03:55:52 +0900

    #21 完了

--- Assets/Shooting_Game/Create_JavaScript/BackMemu.js
+++ Assets/Shooting_Game/Create_JavaScript/BackMemu.js
@@ -4,7 +4,24 @@ function Update () {
   if(Input.GetKey("b"))
   {
     Application.LoadLevel(0) ;
+    if(Application.loadedLevelName == "Asset_Links")
+    {
+      Application.LoadLevel("Credit") ;
+    }
+    if(Application.loadedLevelName == "Material")
+    {
+      Application.LoadLevel("Asset_Links") ;
+    }
+    if(Application.loadedLevelName == "Work_assistance")
+    {
+      Application.LoadLevel("Material") ;
+    }
+    if(Application.loadedLevelName == "Unity")
+    {
+      Application.LoadLevel("Material") ;
+    }
   }
+
   if(Input.GetKey("n"))
   {
     if(Application.loadedLevelName == "Introduce")
@@ -14,6 +31,22 @@ function Update () {
     if(Application.loadedLevelName == "Credit")
     {
       Application.LoadLevel("Asset_Links") ;
+    }
+    if(Application.loadedLevelName == "Asset_Links")
+    {
+      Application.LoadLevel("Material") ;
+    }
+    if(Application.loadedLevelName == "Material")
+    {
+      Application.LoadLevel("Work_assistance") ;
+    }
+    if(Application.loadedLevelName == "Work_assistance")
+    {
+      Application.LoadLevel("Unity") ;
+    }
+    if(Application.loadedLevelName == "Unity")
+    {
+      Application.LoadLevel(0) ;
     }
   }
 
