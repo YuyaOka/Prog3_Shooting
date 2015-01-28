03a5ec3da0b0db6e557c33010f47c9c759b77572
Yuya Oka <YuyaOka@users.noreply.github.com>
2015-01-28 15:23:25 +0900

    HASAMA2Box.js #22 修正

--- Assets/Shooting_Game/Create_JavaScript/HASAMA2Box.js
+++ Assets/Shooting_Game/Create_JavaScript/HASAMA2Box.js
@@ -1,11 +1,18 @@
 #pragma strict
 
+var kurae : AudioClip ;
+var cnt : int = 0 ;
 var Enemy : Transform ;
 
 function Update ()
 {
   if(Time.frameCount % 60 == 0)
   {
+    cnt += 1 ;
     Instantiate(Enemy , transform.position , transform.rotation) ;
+    if(cnt % 15 == 0)
+    {
+        AudioSource.PlayClipAtPoint(kurae , transform.position , transform.rotation) ;
+    }
   }
 }
