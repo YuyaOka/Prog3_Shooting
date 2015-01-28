ae834c1feaab75f4ef90a1c04bad9b9e9bb8ca71
YuyaOka <kigyouka@MacBook-Pro.local>
2015-01-28 17:26:45 +0900

    #22 修正

--- Assets/Shooting_Game/Create_JavaScript/GameOver.js
+++ Assets/Shooting_Game/Create_JavaScript/GameOver.js
@@ -1,6 +1,7 @@
 #pragma strict
 
 var customLabel : GUIStyle ;
+var restart : AudioClip ;
 
 function Start() {
   if(Score.Score > HighScore.High)
@@ -18,7 +19,8 @@ function Update() {
   /* キーボードからの入力 */
   if(Input.GetKey("r"))
   {
-    Application.LoadLevel("Main") ;
+    AudioSource.PlayClipAtPoint(restart , transform.position) ;
+    Invoke("game" , 3.5) ;
     Counter.Count = 0 ;
   }
 
@@ -40,7 +42,12 @@ function Update() {
 
   if(Input.GetKey("joystick button 9"))
   {
-    Application.LoadLevel("Main") ;
+    AudioSource.PlayClipAtPoint(restart , transform.position) ;
+    Invoke("game" , 3.5) ;
     Counter.Count = 0 ;
   }
+}
+
+function game () {
+  Application.LoadLevel("Main") ;
 }
--- Assets/Shooting_Game/Create_JavaScript/HASAMA2Box.js
+++ Assets/Shooting_Game/Create_JavaScript/HASAMA2Box.js
@@ -6,13 +6,13 @@ var Enemy : Transform ;
 
 function Update ()
 {
-  if(Time.frameCount % 60 == 0)
+  if(Time.frameCount % 80 == 0)
   {
     cnt += 1 ;
     Instantiate(Enemy , transform.position , transform.rotation) ;
     if(cnt % 15 == 0)
     {
-        AudioSource.PlayClipAtPoint(kurae , transform.position , transform.rotation) ;
+        AudioSource.PlayClipAtPoint(kurae , transform.position) ;
     }
   }
 }
